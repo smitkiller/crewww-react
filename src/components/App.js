a1e1ef8498142c94688245e72e9901fef5123fff
smitkiller <wsxmit@gmail.com>
2017-08-03 15:54:30 +0700

    edit app page

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
       <Header header='App'/>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
       </div>
     );
