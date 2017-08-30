2bc16233838c1f3cf7161a37d75b38e28dc8579c
smitkiller <wsxmit@gmail.com>
2017-08-30 13:01:09 +0700

    Test new routes

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,41 +1,83 @@
-import React from 'react';
-import { Router, Route,IndexRoute } from 'react-router'
+import React,{Component} from 'react';
+import { Route, BrowserRouter, Link, Redirect, Switch } from 'react-router-dom'
 import { syncHistoryWithStore } from 'react-router-redux'
 
-
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import RaisedButton from 'material-ui/RaisedButton'
+import MenuItem from 'material-ui/MenuItem'
+import logo from '../components/img/logo2.png'
 import App from '../components/App'
 import Home from '../containers/Home'
 import About from '../components/About'
-import simpleForm from '../components/simpleForm'
 import Pages from '../containers/Pages/index'
 import NewPage from '../containers/Pages/New'
 import ShowPage from '../containers/Pages/Show'
 import EditPage from '../containers/Pages/Edit'
 import Login from '../containers/Login'
-import {requireAuthentication} from '../components/AuthenticatedComponent'
-import {ProtectedView} from '../components/ProtectedView'
 
-
-
-export default (store, history) => {
+function PrivateRoute ({component: Component, authed, ...rest}) {
   return (
-    <Router history={syncHistoryWithStore(history, store)}>
-    <Route path="/" component={Home} />
-    <Route path="app" component={App} />
-    <Route path="about" component={About}/>
-    <Route path="home" component={Home}/>
-    <Route path="login" component={Login}/>
-    <route path='pages'>
-      <IndexRoute component={Pages} />
-      <route path='new'
-             component={NewPage} />
-      <route path=':id'
-             component={ShowPage} />
-     <route path='edit'>
-            <route path=':id' component={EditPage} />
-      </route>
-    </route>
+    <Route
+      {...rest}
+      render={(props) => authed === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}/>
+  )
+}
 
-  </Router>
-)
+function PublicRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === false
+        ? <Component {...props} />
+        : <Redirect to='/home' />}/>
+  )
+}
+
+export default class Routes extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      open:false
+    }
+  }
+  handleToggle = () =>this.setState({
+    open:!this.state.open
+  })
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+        <AppBar
+        title="Crewww-react"
+        onLeftIconButtonTouchTap={this.handleToggle}
+        iconElementRight = {<img src={logo} />}/>
+        {/*iconElementRight =*/} {/*<img src='header-logo.png' alt="Logo" />*/}
+        <div>
+        <Drawer
+        width={200}
+        openSecondary={false}
+        open={this.state.open}>
+        <AppBar title="test" onLeftIconButtonTouchTap={this.handleToggle}/>
+        <Link style={{textDecoration:'none'}} to={'/home'}><MenuItem>Home</MenuItem></Link>
+        <Link style={{textDecoration:'none'}} to={'/app'}><MenuItem>App</MenuItem></Link>
+        <Link style={{textDecoration:'none'}} to={'/pages'}><MenuItem>Pages</MenuItem></Link>
+        <Link style={{textDecoration:'none'}} to={'/login'}><MenuItem>Login</MenuItem></Link>
+        {/*<Link style={{textDecoration:'none'}} to={'/about'}><MenuItem>About</MenuItem></Link>*/}
+        </Drawer>
+        </div>
+          <div className="container">
+            <div className="row">
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <PrivateRoute authed={true} path="/app" component={App} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
