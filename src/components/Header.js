2c490d454906359f715da35b9376bbf49d8aba76
smitkiller <30369377+smitkiller@users.noreply.github.com>
2017-09-15 16:11:51 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,8 @@ import RaisedButton from 'material-ui/RaisedButton'
 import MenuItem from 'material-ui/MenuItem'
 import { Link} from 'react-router'
 import logo from './img/logo2.png'
+import {logout} from '../Auth/auth'
+import { firebaseAuth } from '../constants/configAuth'
 
 class Header extends Component {
   constructor(props){
@@ -13,6 +15,7 @@ class Header extends Component {
       open:false
     }
   }
+
   handleToggle = () =>this.setState({
     open:!this.state.open
   })
@@ -32,9 +35,17 @@ class Header extends Component {
           open={this.state.open}>
           <AppBar title={header} onLeftIconButtonTouchTap={this.handleToggle}/>
           <Link style={{textDecoration:'none'}} to={'/home'}><MenuItem>Home</MenuItem></Link>
-          <Link style={{textDecoration:'none'}} to={'/app'}><MenuItem>App</MenuItem></Link>
+          <Link style={{textDecoration:'none'}} to={'/app'} ><MenuItem>App</MenuItem></Link>
           <Link style={{textDecoration:'none'}} to={'/pages'}><MenuItem>Pages</MenuItem></Link>
-          <Link style={{textDecoration:'none'}} to={'/login'}><MenuItem>Login</MenuItem></Link>
+         
+          {this.state.authed
+                    ? <button
+                        style={{border: 'none', background: 'transparent'}}
+                        onClick={() => {
+                          logout()
+                        }}
+                        className="navbar-brand">Logout</button>
+                    : <Link style={{textDecoration:'none'}} to={'/login'}><MenuItem>Login</MenuItem></Link>}
           {/*<Link style={{textDecoration:'none'}} to={'/about'}><MenuItem>About</MenuItem></Link>*/}
           </Drawer>
           </div>
@@ -42,4 +53,4 @@ class Header extends Component {
    )
  }
 }
-export default Header;
+export default Header
