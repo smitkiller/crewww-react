245f694222e23daebb75e36923bc09a4c57de5ca
smitkiller <30369377+smitkiller@users.noreply.github.com>
2017-09-16 12:54:37 +0700

    Add files via upload

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,5 +1,5 @@
-import {createReducer} from '../utils'
 import {pushState} from 'redux-router'
+import { browserHistory } from 'react-router'
 import {
   LOGIN_USER_REQUEST,
   LOGIN_USER_SUCCESS,
@@ -11,7 +11,6 @@ const initialState = {
   token: null,
   userName: null,
   isAuthenticated: false,
-  isAuthenticating: false,
   statusText: null
 }
 
@@ -19,18 +18,17 @@ export default (state = initialState, action) => {
   switch(action.type) {
     case LOGIN_USER_SUCCESS:
       return Object.assign({}, state, {
-          'isAuthenticating': false,
           'isAuthenticated': true,
-          'token': action.payload.token,
+          'userName':action.name,
+          'token': true,
           'statusText': 'You have been successfully logged in.'
       });
     case LOGIN_USER_FAILURE:
       return Object.assign({}, state, {
-          'isAuthenticating': false,
           'isAuthenticated': false,
           'token': null,
           'userName': null,
-          'statusText': 'Authentication Error'
+          'statusText': 'Invalid username/password.'
       });
     default:
       return state
