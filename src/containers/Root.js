c5c027a5b108e67b6115f65f50b615185abcee77
smit <wsxmit@gmail.com>
2017-09-19 09:42:56 +0700

    add hot reload

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -6,7 +6,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import theme from '../components/material_ui_raw_theme_file'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
-injectTapEventPlugin();
+//injectTapEventPlugin();
 
 export default class Root extends Component {
   render() {
@@ -14,15 +14,14 @@ export default class Root extends Component {
     const store = configureStore(history, initialState)
 
     return (
-      
-      <MuiThemeProvider muiTheme={theme}>
       <Provider store={store} key='provider'>
-        <Routes
-        store={store}
-         history={history}
-        />
-     </Provider>
+      <MuiThemeProvider muiTheme={theme}>     
+          <Routes
+           store={store}
+           history={history}
+          />
       </MuiThemeProvider>
+       </Provider>
        
     )
   }
