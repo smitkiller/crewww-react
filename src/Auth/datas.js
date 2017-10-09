dbc0102f164023a34e6dd98603b7b794c253693f
smitkiller <30369377+smitkiller@users.noreply.github.com>
2017-10-09 19:44:07 +0700

    Delete datas.js

--- src/Auth/datas.js
+++
@@ -1,9 +0,0 @@
-import { ref, firebaseAuth } from '../constants/configAuth'
-
-
-export function getDatas() {
-  return ref.child(`pages`).once('value',snap =>{
-  	const datas = snap.val();
-  //	console.log('xxxxxxxxxxxxxxxxxxxx',datas)
-  })
-}
