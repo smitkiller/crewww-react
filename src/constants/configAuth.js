d0a02c46bd33d1e74fb00de0cfd9135bda3df25b
smitkiller <30369377+smitkiller@users.noreply.github.com>
2019-11-10 20:14:29 +0700

    Update configAuth.js

--- src/constants/configAuth.js
+++ src/constants/configAuth.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 
 const config = {
-  apiKey: 'AIzaSyA2PTRbHj5lUyoN0CjkG8vb5vOXwssXj40',
-  authDomain: 'crewww-c780d.firebaseapp.com',
-  databaseURL: 'https://crewww-c780d.firebaseio.com',
-  projectId: 'crewww-c780d',
-  storageBucket: 'gs://crewww-c780d.appspot.com/'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: ''
 };
 firebase.initializeApp(config)
 //export const database= firebase.database();
